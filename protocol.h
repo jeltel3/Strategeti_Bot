dd617e0bb5b7a6dbe1a172b5aadb330a43a416ba
jeltel3 <jelte.loeve@gmail.com>
2026-07-02 08:56:28 +0200

    Refactor 'go' command to include time management
    Updated the command handling for 'go' to parse time left for both players.

--- protocol.h
+++ protocol.h
@@ -12,7 +12,7 @@ int main(){
     position board;
     while(true){
         string line;
-        cin>>line;
+        getline(cin,line);
         if(line=="quit"){
             break;
         }
@@ -23,10 +23,19 @@ int main(){
         else if(line=="isready"){
             cout<<"readyok"<<endl;
         }
-        else if(line=="go"){
-            Move bestmove=bestMove(board);
+        else if(line.substr(0,2)=="go{
+            int wtime=1e9, btime=1e9; //time left for white in milliseconds and time left for black in milliseconds
+            if(line.find("wtime")!=string::npos){
+                stringstream s(line.substr(line.find("wtime")+6));
+                s>>wtime;
+            }
+            if(line.find("btime")!=string::npos){
+                stringstream s(line.substr(line.find("btime")+6));
+                s>>btime;
+            }
+            Move bestmove=bestMove(board); //any debug prints up to 10000 characters with an endline at the end is fine in the bestMove function, as long as it does not contain the string bestmove
             makeMove(board,bestmove);
-            cout<<moveToUSI(bestmove)<<endl;
+            cout<<"bestmove "<<moveToUSI(bestmove)<<endl;
         }
         else{
             makeMove(board,USItoMove(line,board));
